9fe53d4722fec7d72a81da07869367f159eba1cb
deuccalionn <164162455+deuccalionn@users.noreply.github.com>
2025-11-19 22:49:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ if st.button("Bunu Benim İçin Sadeleştir"):
     else:
         try:
             genai.configure(api_key=api_key)
-            model = genai.GenerativeModel('gemini-1.5-flash')
+            model = genai.GenerativeModel('gemini-pro')
             
             with st.spinner('Yapay zeka metni inceliyor...'):
                 prompt = f"""
